b9877ec1a9cb0a35fa1329f09dcf8f1b24caf525
Galina Avdeeva <cornhedgehog@gmail.com>
2017-01-23 19:04:52 +0200

    Добавьте файлы проекта.

---
+++ pattern_examples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("changes");
+        }
+    }
+}
